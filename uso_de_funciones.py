bc45426eeb4a792f8ddfde20c44ab376b9f864d6
KarolRa11 <152220865+KarolRa11@users.noreply.github.com>
2025-01-21 21:38:41 -0600

    funciones

---
+++ uso_de_funciones.py
@@ -0,0 +1,18 @@
+#Uso de funciones en python
+# 1.- Función que salude
+# 2.- Función que recibe 2 argumentos y hace una operación matemática
+
+#Ejercicio 1
+def saludar():
+    print("Hola, este es un saludo")
+
+saludar()
+
+#Ejercicio 2
+def suma(numero_uno,numero_dos):
+    suma = numero_uno + numero_dos
+    print(suma)
+
+suma(10,5)
+
+#Ejercicio 3 - Pedir al usuario 2 número y pasarlos como argumentos y realizar una suma
